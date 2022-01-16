fe56525dd1d258f399ca37cc66f5289821299d28
Rickard <rickard.borg1990@gmail.com>
2022-01-16 21:16:44 +0100

    second commit

---
+++ file-manager-application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace file_manager_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
